f3b575941d7b15dc7d97ec94bb868dcb50f8a014
DiegoTardeli <tardelli.assuncao@escolar.ifrn.edu.br>
2025-08-12 16:51:43 -0300

    Conclusao
    Atividade concluída.

--- BrackeysAtividade2D/Assets/sprites/brackeys_platformer_assets/Scripts/Player.cs
+++ BrackeysAtividade2D/Assets/sprites/brackeys_platformer_assets/Scripts/Player.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
     public float velocidade = 40;
     public float forcaDoPulo = 4;
-    
+
     private bool noChao = false;
-    private bool andando = false;
-    
+    private bool correndo = false;
+    private bool rolando = false;
+
     private SpriteRenderer sprite;
     private Rigidbody2D rb;
     private Animator animator;
-    
+
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
@@ -21,39 +21,47 @@ public class Player : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-       
     void Update()
     {
-        andando = false;
-        
+        correndo = false;
+        rolando = false;
+
+        // Movimento para esquerda
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
+            transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
             sprite.flipX = true;
-            andando = true;
+            correndo = true;
         }
-        
+
+        // Movimento para direita
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
+            transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
             sprite.flipX = false;
-            andando = true;
+            correndo = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
+        // Pulo
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
         {
-            rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(0, forcaDoPulo), ForceMode2D.Impulse);
         }
 
-        animator.SetBool("Andando",andando);
-        animator.SetBool("Pulo",!noChao);
-        
+        // Rolamento (exemplo com Shift)
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rolando = true;
+        }
+
+        // Atualiza os parâmetros do Animator
+        animator.SetBool("Correndo", correndo);
+        animator.SetBool("Rolamento", rolando);
     }
 
     void OnCollisionEnter2D(Collision2D colisao)
     {
-        //if (colisao.gameObject.tag == "Chao")
-        if(colisao.gameObject.CompareTag("Chao"))
+        if (colisao.gameObject.CompareTag("Chao"))
         {
             noChao = true;
         }
@@ -61,11 +69,9 @@ public class Player : MonoBehaviour
 
     void OnCollisionExit2D(Collision2D colisao)
     {
-        if(colisao.gameObject.CompareTag("Chao"))
+        if (colisao.gameObject.CompareTag("Chao"))
         {
             noChao = false;
         }
     }
-    
 }
-
